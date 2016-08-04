ac2f614bdb9d95ec7440571a0e0515ca4fbbfc86
StephenGrable1 <stephengrable@gmail.com>
2016-08-04 16:46:14 -0400

    made the home page the default route by adding urlRouteProvider to my app.config in main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var app = angular.module('Single-Page-App', ['ui.router', 'ngAnimate', 'ngMessag
 
 
 app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/home');
 	$stateProvider
 	.state("home", {
 		url:"/home",

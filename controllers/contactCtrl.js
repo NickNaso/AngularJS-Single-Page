823da3d3a6209e39117dcfce1c13b090783fc238
StephenGrable1 <stephengrable@gmail.com>
2016-08-04 19:23:35 -0400

    created http object in the contactCtrl.js to send input values to the process-form.php script

--- controllers/contactCtrl.js
+++ controllers/contactCtrl.js
@@ -1,9 +1,29 @@
 angular
 	.module('Single-Page-App')
-	.controller('contactCtrl', function($scope){
+	.controller('contactCtrl', function($scope, $http){
 		$scope.contact = {};
 
-		$scope.processForm = function() {
-        alert('awesome!'); 
-        }; 
-	});
+		$scope.contact = {name : '', email : '', subject : '', message : ''};
+		$scope.submitForm = function() {
+     		var config = {
+				method: 'POST',
+				url : '../php/process-form.php',
+				data : {
+					'name' : $scope.contact.name,
+					'email': $scope.contact.email,
+					'subject': $scope.contact.subject,
+					'message' : $scope.contact.message
+				}
+			};
+			var request = $http(config);
+			request.then(function (response){
+				$scope.msg = response.data;
+				console.log($scope.msg);
+			}, function (error){
+				$scope.msg = error.data;
+				console.log($scope.msg);
+
+		});
+	}
+});
+

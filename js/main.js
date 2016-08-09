ddc718064bebd6b3e7999ca89a5029fc566e26e1
StephenGrable1 <stephengrable@gmail.com>
2016-08-09 17:27:31 -0400

    deleted window element to for troubleshooting in javascript

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@
 Main AngularJS Single-Page Web App
 */
 
-var w = window.innerWidth;
-
-console.log(w);
-
 var app = angular.module('Single-Page-App', ['ui.router', 'ngAnimate', 'ngMessages']);
 
 

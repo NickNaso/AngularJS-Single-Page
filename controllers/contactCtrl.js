04744a044f4ac48318d40f0b765d4b6655918c02
StephenGrable1 <stephengrable@gmail.com>
2016-08-30 17:41:44 -0400

    fixed dependencies for heroku minification

--- controllers/contactCtrl.js
+++ controllers/contactCtrl.js
@@ -1,8 +1,8 @@
 angular
 	.module('Single-Page-App')
-	.controller('contactCtrl', function($scope, $http){
+	.controller('contactCtrl', [$scope, $http, function($scope, $http){
 		$scope.contact = {name : '', email : '', subject : '', message : ''};
-		
+
 
 		$scope.submitForm = function() {
      		var config = {
@@ -19,7 +19,7 @@ angular
 			request.then(function (response){
 
 				if(typeof(response.data) == 'string') {
-					// make all error messages blank when 
+					// make all error messages blank when
 					// php return a string (which is the success message)
 					// which means there are no error messages being sent from php
 					$scope.nameError = "";
@@ -27,15 +27,15 @@ angular
 					$scope.subjectError = "";
 					$scope.emailError = "";
 
-					// put the success string from php into 
+					// put the success string from php into
 					// the successMsg so it can be accessed in the view
 					$scope.successMsg = response.data;
 
-					// clear all form values 
-					// and set the inputs to prisitine and untouched 
-					// so that angular will not display any error messages 
-					// once a user submits the form successfully 
-					
+					// clear all form values
+					// and set the inputs to prisitine and untouched
+					// so that angular will not display any error messages
+					// once a user submits the form successfully
+
 					$scope.contact = {};
 					$scope.contactForm.$setPristine();
 					$scope.contactForm.$setUntouched();
@@ -44,10 +44,10 @@ angular
 					console.log("not an object");
 				} else {
 					if(typeof(response.data) == 'object') {
-					// if php sends an object 
-					// (which contains all the error messages present) 
+					// if php sends an object
+					// (which contains all the error messages present)
 					// populate variables with error messages
-					
+
 					$scope.nameError = response.data['name-error'];
 					$scope.messageError = response.data['message-error'];
 					$scope.subjectError = response.data['subject-error'];
@@ -59,16 +59,15 @@ angular
 					console.log("it is an object");
 					}
 				}
-				
-				
+
+
 			}, function (error){
 				$scope.msg = error.data;
 				console.log($scope.msg);
 
 		});
 	}
-});
+}]);
 
-//Helpful video about sending data from angular form to php 
+//Helpful video about sending data from angular form to php
 //https://www.youtube.com/watch?v=gVd6Mo-VEQg
-

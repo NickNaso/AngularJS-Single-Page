6d9941a3d97633479c7adbef1e4ab26174d1381c
StephenGrable1 <stephengrable@gmail.com>
2016-09-01 11:06:53 -0400

    created agular.boostrap and removed ng-app

--- js/main.js
+++ js/main.js
@@ -1,51 +1,41 @@
 var app = angular.module('Single-Page-App', ['ui.router', 'ngMessages']);
 
+
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('/home');
-
-	var home = {
-		name: 'home',
-		url: '/home',
-		views: {
-			"main@": {
-				templateUrl: "partials/home.html"
+	$stateProvider
+		.state("home", {
+			url:"/home",
+			views: {
+				"main@": {
+					templateUrl: "partials/home.html"
+				}
 			}
-		}
-	}
-
-	var listen = {
-		name: 'listen',
-		url: '/listen',
-		views: {
-			"main@": {
-				templateUrl: "partials/listen.html"
+		})
+		.state("listen", {
+			url:"/listen",
+			views: {
+				"main@": {
+					templateUrl: "partials/listen.html"
+				}
 			}
-		}
-	}
-
-	var watch = {
-		name: 'watch',
-		url: '/watch',
-		views: {
-			"main@": {
-				templateUrl: "partials/watch.html"
+		})
+		.state("watch", {
+			url:"/watch",
+			views: {
+				"main@": {
+					templateUrl: "partials/watch.html"
+				}
 			}
-		}
-	}
-
-	var contact = {
-		name: 'contact',
-		url: '/contact',
-		views: {
-			"main@": {
-				templateUrl: "partials/contact.html"
+		})
+		.state("contact", {
+			url:"/contact",
+			views: {
+				"main@": {
+					templateUrl: "partials/contact.html"
+				}
 			}
-		}
-	}
+		})
+	}])
 
-	$stateProvider.state(home);
-	$stateProvider.state(listen);
-	$stateProvider.state(watch);
-	$stateProvider.state(contact);
-
-}])
+angular.bootstrap(document, ['Single-Page-App']);

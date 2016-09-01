4351e23c621796dd66ce3909e46aa8c81bd8a731
StephenGrable1 <stephengrable@gmail.com>
2016-09-01 19:21:58 -0400

    Fix Firefox bug

--- js/main.js
+++ js/main.js
@@ -1,57 +1,41 @@
 var app = angular.module('Single-Page-App', ['ui.router', 'ngMessages']);
 
-
-
 app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 	$urlRouterProvider.otherwise('/home');
-
-	var home = {
-	    name: 'home',
-	    url: '/home',
-	    views: {
-				"main@": {
-					templateUrl: "partials/home.html"
-				}
+	$urlRouterProvider.otherwise('/home');
+	$stateProvider
+	.state("home", {
+		url:"/home",
+		views: {
+			"main@": {
+				templateUrl: "partials/home.html"
 			}
-	  }
-
-	  var listen = {
-	    name: 'listen',
-	    url: '/listen',
-	    views: {
-				"main@": {
-					templateUrl: "partials/listen.html"
-				}
+		}
+	})
+	.state("listen", {
+		url:"/listen",
+		views: {
+			"main@": {
+				templateUrl: "partials/listen.html"
 			}
-	  }
-
-		var watch = {
-	    name: 'watch',
-	    url: '/watch',
-	    views: {
-				"main@": {
-					templateUrl: "partials/watch.html"
-				}
+		}
+	})
+	.state("watchHere", {
+		url:"/watch",
+		views: {
+			"main@": {
+				templateUrl: "partials/watch.html"
 			}
-	  }
-
-
-		var contact = {
-	    name: 'contact',
-	    url: '/contact',
-			controller: '../controllers/contactCtrl',
-	    views: {
-				"main@": {
-					templateUrl: "partials/contact.html"
-				}
+		}
+	})
+	.state("contact", {
+		url:"/contact",
+		views: {
+			"main@": {
+				templateUrl: "partials/contact.html"
 			}
-	  }
-
-	  $stateProvider.state(home);
-	  $stateProvider.state(listen);
-		$stateProvider.state(watch);
-		$stateProvider.state(contact);
-
+		}
+	})
 }])
 
 angular.bootstrap(document, ['Single-Page-App']);

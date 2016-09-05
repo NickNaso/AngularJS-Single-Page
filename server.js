274a6d9516352147be3117dc7d1bbdf4c38b68bf
Nicola Del Gobbo <nicoladelgobbo@gmail.com>
2016-09-05 22:38:29 +0200

    Changes in server to serve Angular App
    Some changes for https://github.com/expressjs/express/issues/3075

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
@@ -5,10 +6,16 @@ var port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
-app.get('/', function(req, res) {
+
+app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.get('/', function (req, res) {
+  res.render('index');
+});
+/*app.get('/', function(req, res) {
 
     res.sendfile('index.html', {root: __dirname })
-});
+});*/
 
 app.listen(port, function() {
   console.log('Our app is running on port ' + port);
